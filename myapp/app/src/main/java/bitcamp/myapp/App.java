4933aa9fb31345791ccd3212510f07a0bc8cabf1
parktrue <pomul@naver.com>
2023-06-01 16:48:18 +0900

    day8

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -5,69 +5,99 @@ import java.util.Scanner;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("나의 목록 관리 시스템");
-    System.out.printf("\n------------------------\n");
-
     // 키보드 스캐너 준비
     // 각 데이터를 키보드를 통해 입력받아서 출력한다.
     Scanner scanner = new java.util.Scanner(System.in);
 
-    final int SIZE = 100; // final 변수는 값이 한번 들어가면 값을 바꿀 수 없다.
+    final int MAX_SIZE = 100; // final 변수는 값이 한번 들어가면 값을 바꿀 수 없다.
+    int userId = 1;
+    int length = 0;
 
-    int[] no = new int[SIZE]; // 3은 메모리의 개수이며 int는 만들 메모리의 데이터 타입이다. no는 배열의 주소를 담는 레퍼런스이다.
-    String[] name = new String[SIZE];
-    int[] age = new int[SIZE];
-    boolean[] working = new boolean[SIZE];
-    char[] gender = new char[SIZE];
-    float[] leftEye = new float[SIZE];
-    float[] rightEye = new float[SIZE];
+    // int[] no = new int[MAX_SIZE]; // 3은 메모리의 개수이며 int는 만들 메모리의 데이터 타입이다. no는 배열의
+    // 주소를 담는 레퍼런스이다.
+    int[] no = new int[MAX_SIZE];
+    String[] name = new String[MAX_SIZE];
+    String[] email = new String[MAX_SIZE];
+    String[] password = new String[MAX_SIZE];
+    char[] gender = new char[MAX_SIZE];
+    printTitle();
 
-    for (int i = 0; i < SIZE; i++) {
+    // 회원 정보 등록
+    for (int i = 0; i < MAX_SIZE; i++) {
       // i++; // 값을 1개 증가 == i += 1;, i = i + 1;
-
-      System.out.print("번호 : ");
-      no[i] = scanner.nextInt();
-
-      System.out.print("이름 : ");
-      name[i] = scanner.next();
-
-      System.out.print("나이 : ");
-      age[i] = scanner.nextInt();
-
-      System.out.print("재직중 (true/false) : ");
-      working[i] = scanner.nextBoolean();
-
-      System.out.print("성별 (남자:M, 여자:W) : ");
-      String str = scanner.next();
-      gender[i] = str.charAt(0);
-
-      System.out.print("시력 (왼쪽, 오른쪽) : ");
-      leftEye[i] = scanner.nextFloat();
-      rightEye[i] = scanner.nextFloat();
+      inputMember(scanner, i, name, email, password, gender, no, userId++);
+      length++;
+      if (!promptContinue(scanner)) {
+        break;
+      }
     }
 
-    System.out.println("\n----------------------\n");
+    printMembers(length, no, name, email, gender);
+    scanner.close();
+  }
 
-    // int no = SIZE;
-    // String name = "홍길동"; // name는 레퍼런스 변수이다.
-    // int age = 20;
-    // boolean working = true;
-    // char gender = 'M';
-    // float leftEye = 1.5f;
-    // float rightEye = 1.0f; // camel 표기법, 파라미터명은 소문자 시작, class명은 대문자로 시작
+  static void printTitle() {
+    System.out.println("나의 목록 관리 시스템");
+    System.out.println("\n-------------------------------\n");
+  }
 
-    for (int i = 0; i < SIZE; i++) {
-      System.out.printf("번호: %d\n", no[i]);
+  static void inputMember(
+      Scanner scanner,
+      int i,
+      String[] name,
+      String[] email,
+      String[] password,
+      char[] gender,
+      int[] no,
+      int userId) {
+    System.out.print("이름 : ");
+    name[i] = scanner.next();
 
-      System.out.printf("이름: %s\n", name[i]);
+    System.out.print("이메일 : ");
+    email[i] = scanner.next();
 
-      System.out.printf("나이: %d\n", age[i]); // 나이라는 문자열과 20이라는 정수를 합쳐서 하나의 문자열로 전달
+    System.out.print("암호 : ");
+    password[i] = scanner.next();
 
-      System.out.printf("재직자 : %b\n", working[i]);
+    loop:
+    while (true) {
+      System.out.println("성별 : ");
+      System.out.println(" 1. 남자");
+      System.out.println(" 2. 여자");
+      System.out.print("> ");
+      String menuNo = scanner.next();
+      scanner.nextLine(); // 입력 값(token) 읽고 난 후에 남아있는 줄바꿈 코드를 제거한다.
+      switch (menuNo) {
+        case "1":
+          gender[i] = 'M';
+          break loop;
+        case "2":
+          gender[i] = 'W';
+          break loop;
+        default:
+          System.out.println("무효한 번호입니다.");
+      }
+    }
+    no[i] = userId++;
+  }
 
-      System.out.printf("성별: (남자(M), 여자(W)): %c\n", gender[i]);
+  static boolean promptContinue(Scanner scanner) {
+    System.out.print("계속 하시겠습니까?(Y/n) ");
+    String response = scanner.nextLine();
+    // equalsIgnoreCase 는 대소문자를 구분하지 않겠다는 의미이다.
+    if (!response.equals("") && !response.equalsIgnoreCase("Y")) {
+      return false;
+    }
+    return true;
+  }
 
-      System.out.printf("좌우시력: %.1f, %.1f\n", leftEye[i], rightEye[i]);
+  static void printMembers(int length, int[] no, String[] name, String[] email, char[] gender) {
+    System.out.println("\n-------------------------------\n");
+
+    System.out.println("번호, 이름, 이메일, 성별\n");
+    System.out.println("-----------나의 목록-----------");
+    for (int i = 0; i < length; i++) {
+      System.out.printf("%d, %s, %s, %c\n", no[i], name[i], email[i], gender[i]);
     }
   }
 }
