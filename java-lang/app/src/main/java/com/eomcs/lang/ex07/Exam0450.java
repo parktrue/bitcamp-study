cfa8897608c0a4d736471b77960a08d4dcbb00f3
parktrue <pomul@naver.com>
2023-06-02 17:41:54 +0900

    day9

--- java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0450.java
+++ java-lang/app/src/main/java/com/eomcs/lang/ex07/Exam0450.java
@@ -1,12 +1,11 @@
 package com.eomcs.lang.ex07;
 
-//# 메서드 : 스택 메모리 응용 II - 재귀호출
+// # 메서드 : 스택 메모리 응용 II - 재귀호출
 //
 public class Exam0450 {
 
   static int sum(int value) {
-    if (value == 1)
-      return 1;
+    if (value == 1) return 1;
 
     return value + sum(value - 1);
   }
@@ -14,7 +13,7 @@ public class Exam0450 {
   public static void main(String[] args) {
     // 다음과 같이 작은 수를 계산 할 때는 재귀호출을 사용하는 것이
     // 코드도 간단하고 이해하기도 쉽다.
-    System.out.println(sum(18289));
+    System.out.println(sum(25));
   }
 }
 // JVM Stack 메모리의 사용
@@ -43,5 +42,3 @@ public class Exam0450 {
 //   재귀호출을 할 때 스택오버플로우가 자주 발생한다.
 // - 메서드 호출이 너무 깊게 들어가지 않는 상황에서 재귀호출을 사용하라.
 //
-
-
