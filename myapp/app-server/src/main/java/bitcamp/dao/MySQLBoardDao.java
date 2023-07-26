e8e847624ce89b2d43e1dc87a89013ac21171066
parktrue <pomul@naver.com>
2023-07-26 19:27:09 +0900

    day46 (DB커넥션풀을 이용한 Connection 재사용)

--- myapp/app-server/src/main/java/bitcamp/dao/MySQLBoardDao.java
+++ myapp/app-server/src/main/java/bitcamp/dao/MySQLBoardDao.java
@@ -1,6 +1,5 @@
 package bitcamp.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -8,21 +7,22 @@ import java.util.List;
 import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.vo.Board;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.DataSource;
 
 public class MySQLBoardDao implements BoardDao {
 
-  Connection con;
+  DataSource ds;
   int category;
 
-  public MySQLBoardDao(Connection con, int category) {
-    this.con = con;
+  public MySQLBoardDao(DataSource ds, int category) {
+    this.ds = ds;
     this.category = category;
   }
 
   @Override
   public void insert(Board board) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "insert into myapp_board(title,content,writer,password,category)"
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("insert into myapp_board(title,content,writer,password,category)"
             + " values(?,?,?,sha1(?),?)")) {
 
       stmt.setString(1, board.getTitle());
@@ -40,37 +40,16 @@ public class MySQLBoardDao implements BoardDao {
   }
 
   /*
-   select
-     b.board_no,
-     b.title,
-     b.view_count,
-     b.created_date,
-     m.member_no,
-     m.name
-   from
-     myapp_board b inner join myapp_member m on b.writer=m.member_no
-   where
-     category=1
-   order by
-     board_no desc
+   * select b.board_no, b.title, b.view_count, b.created_date, m.member_no, m.name from myapp_board
+   * b inner join myapp_member m on b.writer=m.member_no where category=1 order by board_no desc
    */
   @Override
   public List<Board> list() {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select" +
-            "  b.board_no, " +
-            "  b.title, " +
-            "  b.view_count, " +
-            "  b.created_date, " +
-            "  m.member_no, " +
-            "  m.name " +
-            " from " +
-            "  myapp_board b inner join myapp_member m on b.writer=m.member_no" +
-            " where " +
-            "  category=?" +
-            " order by " +
-            "  board_no desc"
-        )) {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select" + "  b.board_no, " + "  b.title, " + "  b.view_count, "
+            + "  b.created_date, " + "  m.member_no, " + "  m.name " + " from "
+            + "  myapp_board b inner join myapp_member m on b.writer=m.member_no" + " where "
+            + "  category=?" + " order by " + "  board_no desc")) {
 
       stmt.setInt(1, this.category);
 
@@ -101,21 +80,11 @@ public class MySQLBoardDao implements BoardDao {
 
   @Override
   public Board findBy(int no) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select" +
-            "  b.board_no, " +
-            "  b.title, " +
-            "  b.content," +
-            "  b.view_count, " +
-            "  b.created_date, " +
-            "  m.member_no, " +
-            "  m.name " +
-            " from " +
-            "  myapp_board b inner join myapp_member m on b.writer=m.member_no" +
-            " where " +
-            "  category=?" +
-            "  and board_no=?"
-        )) {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select" + "  b.board_no, " + "  b.title, " + "  b.content,"
+            + "  b.view_count, " + "  b.created_date, " + "  m.member_no, " + "  m.name " + " from "
+            + "  myapp_board b inner join myapp_member m on b.writer=m.member_no" + " where "
+            + "  category=?" + "  and board_no=?")) {
 
       stmt.setInt(1, this.category);
       stmt.setInt(2, no);
@@ -134,9 +103,8 @@ public class MySQLBoardDao implements BoardDao {
           writer.setName(rs.getString("name"));
           b.setWriter(writer);
 
-          stmt.executeUpdate("update myapp_board set"
-              + " view_count=view_count + 1"
-              + " where board_no=" + no);
+          stmt.executeUpdate(
+              "update myapp_board set" + " view_count=view_count + 1" + " where board_no=" + no);
 
           return b;
         }
@@ -149,11 +117,8 @@ public class MySQLBoardDao implements BoardDao {
 
   @Override
   public int update(Board board) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "update myapp_board set"
-            + " title=?,"
-            + " content=?"
-            + " where category=? and board_no=? and writer=?")) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement("update myapp_board set"
+        + " title=?," + " content=?" + " where category=? and board_no=? and writer=?")) {
 
       stmt.setString(1, board.getTitle());
       stmt.setString(2, board.getContent());
@@ -170,9 +135,8 @@ public class MySQLBoardDao implements BoardDao {
 
   @Override
   public int delete(Board board) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "delete from myapp_board"
-            + " where category=? and board_no=? and writer=?")) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement(
+        "delete from myapp_board" + " where category=? and board_no=? and writer=?")) {
 
       stmt.setInt(1, this.category);
       stmt.setInt(2, board.getNo());
--- myapp/app-server/src/main/java/bitcamp/dao/MySQLMemberDao.java
+++ myapp/app-server/src/main/java/bitcamp/dao/MySQLMemberDao.java
@@ -1,26 +1,25 @@
 package bitcamp.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
+import bitcamp.util.DataSource;
 
 public class MySQLMemberDao implements MemberDao {
 
-  Connection con;
+  DataSource ds;
 
-  public MySQLMemberDao(Connection con) {
-    this.con = con;
+  public MySQLMemberDao(DataSource ds) {
+    this.ds = ds;
   }
 
   @Override
   public void insert(Member member) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "insert into myapp_member(name,email,password,gender)"
-            + " values(?,?,sha1(?),?)")) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement(
+        "insert into myapp_member(name,email,password,gender)" + " values(?,?,sha1(?),?)")) {
 
       stmt.setString(1, member.getName());
       stmt.setString(2, member.getEmail());
@@ -36,10 +35,9 @@ public class MySQLMemberDao implements MemberDao {
 
   @Override
   public List<Member> list() {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select member_no, name, email, gender"
-            + " from myapp_member"
-            + " order by name asc");
+    try (
+        PreparedStatement stmt = ds.getConnection(false).prepareStatement(
+            "select member_no, name, email, gender" + " from myapp_member" + " order by name asc");
         ResultSet rs = stmt.executeQuery()) {
 
       List<Member> list = new ArrayList<>();
@@ -63,10 +61,9 @@ public class MySQLMemberDao implements MemberDao {
 
   @Override
   public Member findBy(int no) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select member_no, name, email, gender, created_date"
-            + " from myapp_member"
-            + " where member_no=?")) {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select member_no, name, email, gender, created_date"
+            + " from myapp_member" + " where member_no=?")) {
 
       stmt.setInt(1, no);
 
@@ -90,10 +87,9 @@ public class MySQLMemberDao implements MemberDao {
 
   @Override
   public Member findByEmailAndPassword(Member param) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select member_no, name, email, gender, created_date"
-            + " from myapp_member"
-            + " where email=? and password=sha1(?)")) {
+    try (PreparedStatement stmt = ds.getConnection(false)
+        .prepareStatement("select member_no, name, email, gender, created_date"
+            + " from myapp_member" + " where email=? and password=sha1(?)")) {
 
       stmt.setString(1, param.getEmail());
       stmt.setString(2, param.getPassword());
@@ -118,13 +114,8 @@ public class MySQLMemberDao implements MemberDao {
 
   @Override
   public int update(Member member) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "update myapp_member set"
-            + " name=?,"
-            + " email=?,"
-            + " password=sha1(?),"
-            + " gender=?"
-            + " where member_no=?")) {
+    try (PreparedStatement stmt = ds.getConnection(false).prepareStatement("update myapp_member set"
+        + " name=?," + " email=?," + " password=sha1(?)," + " gender=?" + " where member_no=?")) {
 
       stmt.setString(1, member.getName());
       stmt.setString(2, member.getEmail());
@@ -141,8 +132,8 @@ public class MySQLMemberDao implements MemberDao {
 
   @Override
   public int delete(int no) {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "delete from myapp_member where member_no=?")) {
+    try (PreparedStatement stmt =
+        ds.getConnection(false).prepareStatement("delete from myapp_member where member_no=?")) {
 
       stmt.setInt(1, no);
 
